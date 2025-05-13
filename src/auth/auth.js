0f8c2dfa189ed6bb1f1a227266660faea6d8266a
Wallace <wallace.soares@themiind.com>
2025-05-13 17:28:12 -0300

    Banco de dados, Autenticação e Login. Feitos

---
+++ src/auth/auth.js
@@ -0,0 +1,138 @@
+import express from 'express'
+import passport from 'passport'
+import localStrategy from 'passport-local'
+import crypto from 'crypto'
+import { Mongo } from '../database/mongo.js'
+import jwt from 'jsonwebtoken'
+import { ObjectId } from 'mongodb'
+
+const collectName = 'users'
+
+passport.use(new localStrategy({ usernameField: 'email' }, async (email, password, callback) => {
+    const user = await Mongo.db
+        .collection(collectName)
+        .findOne({ email: email })
+
+    if (!user) {
+        return callback(null, false)
+    }
+
+    const saltBuffer = user.salt.saltBuffer.buffer
+
+    crypto.pbkdf2(password, saltBuffer, 310000, 16, 'sha256', (err, hashedPassword) => {
+        if (err) {
+            return callback(null, false)
+        }
+
+        const userPasswordBuffer = Buffer.from(user.password.buffer)
+
+        if (!crypto.timingSafeEqual(userPasswordBuffer, hashedPassword)) {
+            return callback(null, false)
+        }
+
+        const { password, salt, ...rest } = user
+
+        return callback(null, rest)
+
+    })
+}))
+
+const authRouter = express.Router()
+
+authRouter.post('/signup', async (req, res) => {
+    const checkUser = await Mongo.db
+        .collection(collectName)
+        .findOne({ email: req.body.email })
+
+    if (checkUser) {
+        return res.status(500).send({
+            sucess: false,
+            statusCode: 500,
+            body: {
+                text: 'User already exists!'
+            }
+        })
+    }
+
+    const salt = crypto.randomBytes(16)
+    crypto.pbkdf2(req.body.password, salt, 310000, 16, 'sha256', async (err, hashedPassword) => {
+        if (err) {
+            return res.status(500).send({
+                sucess: false,
+                statusCode: 500,
+                body: {
+                    text: 'Error on crypto password',
+                    err: err
+                }
+            })
+        }
+
+        const result = await Mongo.db
+            .collection(collectName)
+            .insertOne({
+                email: req.body.email,
+                password: hashedPassword,
+                salt: { saltBuffer: salt }
+            })
+
+
+        if (result.insertedId) {
+            const user = await Mongo.db
+                .collection(collectName)
+                .findOne({ _id: new ObjectId(result.insertedId) })
+
+            const token = jwt.sign(user, 'secret')
+
+            return res.send({
+                sucess: true,
+                statusCode: 200,
+                body: {
+                    text: 'User Registered Correctly',
+                    token,
+                    user,
+                    logged: true
+                }
+            })
+        }
+    })
+})
+
+authRouter.post('/login', (req, res) => {
+    passport.authenticate('local', (error, user) => {
+        if (error) {
+            return res.status(500).send({
+                sucess: false,
+                statusCode: 500,
+                body: {
+                    text: 'Error during authentication',
+                    error
+                }
+
+            })
+        }
+
+        if(!user) {
+            return res.status(400).send({
+                sucess: false,
+                statusCode: 400,
+                body: {
+                    text: 'User not found'
+                }
+            })
+        }
+
+        const token = jwt.sign(user, 'secret')
+        return res.status(200).send({
+            sucess: true,
+            statusCode: 200,
+            body: {
+                text: 'User logged in correctly',
+                user,
+                token
+            }
+        })
+    })(req, res)
+})
+
+export default authRouter
+// função async é ideal para interação com database
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import cors from 'cors'
+import { Mongo } from './database/mongo.js'
+import { config } from 'dotenv'
+import authRouter from './auth/auth.js'
+
+config()
+
+async function main() {
+    const hostname = 'localhost'
+    const port = 3000
+
+    const app = express()
+
+    const mongoConnection = await Mongo.connect({ mongoConnectionString: process.env.MONGO_CS, mongoDbName: process.env.MONGO_DB_NAME })
+    console.log(mongoConnection)
+    
+    app.use(express.json())
+    app.use(cors())
+
+    app.get('/', (req, res) => {
+        res.send({
+            sucess: true,
+            statusCode: 200,
+            body: 'Bem vindo à loja'
+        })
+    })
+
+    app.use('/auth', authRouter)
+    app.listen(port, () => {
+        console.log(`Server running on: http://${hostname}:${port}`)
+    })
+}
+
+main()
+
+/*
+dotenv = dependencia que permite criar as variáveis de ambiente,
+para não deixar as strings de conexão, senhas... espalhadas pelo código,
+quando for modificar, modifica em apenas um ponto do código.
+*/
